a447735b7816911bff5af4c4ff25bdef7f5240d6
Nicolas Havard <nicolashavard.02@hotmail.fr>
2017-05-06 14:45:58 +0200

    modification ddu header

--- header.php
+++ header.php
@@ -1,14 +1,13 @@
-<header>											
-    <h1>nhavard.ovh</h1>	
-    <div class='button'>
-        <A HREF="home.html"><em>Accueil</em></A><BR />
-        <!--A HREF=""--><em>Applications</em><!--/A--> <BR />
-        <A HREF="parcours.php"<em>Mon parcours</em></A><BR />
-        <!--A HREF=""--><!--/A--> <BR />
-    </div>
-    <div class="language">
-        <img src="flags/UK_flag.jpg" alt="English" title="See the website in English">
-        <img src="flags/FR_flag.jpg" alt="Français" title="Voir le sie en français">
-    </div>
-            										
-</header>
+        <header>                                            
+            <A HREF="http://nhavard.ovh"><h1>nhavard.ovh</h1></A>
+            <div class='button'>
+                <A HREF="http://nhavard.ovh"><em>Accueil</em></A><BR />
+                <A HREF="http://cloud.nhavard.ovh"><em>Cloud </em></A> <BR />
+                <!--A HREF=""--><em>Mail </em><!--/A--> <BR />
+                <!--A HREF=""--><em>Forum </em><!--/A--> <BR />
+            </div>
+            <div class="languages">
+                <A HREF="http://nhavard.ovh/EN"><img src="./flags/UK_flag.png" alt="English" title="See the website in English" width"24" height="16"></A>
+                <A HREF="http://nhavard.ovh"><img src="./flags/FR_flag.png" alt="Français" title="Voir le site en français" width="24" height"16"></A>
+            </div>                                              
+        </header>
