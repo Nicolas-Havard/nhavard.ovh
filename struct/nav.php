333fd96efb289a966516a9b0e6bfde1d8f467481
Nicolas Havard <nicolashavard.02@hotmail.fr>
2017-05-07 23:44:53 +0200

    ajout des icones colores et non colores aux reseaux sociaux, et modifications mineures

---
+++ struct/nav.php
@@ -0,0 +1,10 @@
+<nav>               
+    <H1>Quelques liens :</H1>
+    <ul>
+        <LI><A HREF="http://nhavard.ovh"><em>Accueil</em></A></LI>
+        <LI><A HREF="parcours.html"><em>CV</em></A></LI>  
+        <LI><A HREF="http://cloud.nhavard.ovh"><em>Cloud (en cours)</em></A></LI>
+        <LI><!--A HREF=""--><em>Mail (en projet)</em><!--/A--></LI>
+        <LI><!--A HREF=""--><em>Forum (en projet)</em><!--/A--></LI>
+    </ul>
+</nav>
--- struct/newpage.php
+++ struct/newpage.php
@@ -1,28 +1,30 @@
-<!-- Page d'accueil du site personnel de Nicolas Havard -->
-<!--                Langue française                    -->
-<!--     Dernière mise à jour : le 19/04/2017           -->
-
-
 <!DOCTYPE html>
 <html>
 
     <head>
         <meta charset="utf-8" />		
-        <link rel="stylesheet" href="index.css" />
-        <title>nhavard | Bienvenue</title>										
+        <link rel="stylesheet" href="home.css" />
+        <title>nhavard | </title>										
     </head>
 
 
     <body>
+
         <?php
-            include 'struct/header.php';
+        include 'header.html';
         ?>
-        
-        <div class="page">
-        
-            <?php
-                include 'struct/nav.php';
-            ?>
+
+        <div id="page">
+        <nav>				
+            <H1>Quelques liens :</H1>
+            <ul>
+                <LI><A HREF="home.html"><em>Accueil</em></A></LI>
+                <LI><A HREF="CV.html"><em>CV</em></A></LI>  
+                <LI><!--A HREF=""--><em>Cloud (en cours)</em><!--/A--></LI>
+                <LI><!--A HREF=""--><em>Mail (en projet)</em><!--/A--></LI>
+                <LI><!--A HREF=""--><em>Forum (en projet)</em><!--/A--></LI>
+            </ul>
+        </nav>
         
 
         <section>											
@@ -36,7 +38,7 @@
                     <ul>
                         <li>
                         <strong>2016-2019 : Diplôme d'ingénieur IMA (Informatique, Microélectronique et Automatique)</strong> 
-                        <BR /><A HREF="http://www.polytech-lille.fr/ecole-d-ingenieurs.html#.WLFXnYWcGCo">Polytech</A>, Lille (59)
+                        <BR  /><A HREF="http://www.polytech-lille.fr/ecole-d-ingenieurs.html#.WLFXnYWcGCo">Polytech</A>, Lille (59)
                         </li>           
                         <li>
                         <strong>2014-2016 : Classes préparatoires CPGE MPSI/MP</strong> 
@@ -96,39 +98,29 @@
 
             </aside>
             <article>                						
-                <h1>Bienvenue !</h1>
+                <h1></h1>
        
                 <p>
-                    Bonjour et bienvenue sur mon site. Je m'appelle Nicolas Havard, mais d'autres me connaissent aussi sous le pseudonyme Shuli. 
-                    <BR />
-                </p>
-                <p>
-                    Je suis actuellement étudiant en première année du cycle ingénieur à <A HREF="http://www.polytech-lille.fr/" >Polytech'Lille</A>, dans la filière 
-                    <A HREF="http://www.polytech-lille.fr/informatique-microelectronique-automatique-p118.html" >IMA</A> (Informatique, Microélétronique et Automatique).
-                    Durant mon temps libre, j'aime appronfondir mes connaissances sur l'informatique, l'électronique et la domotique. C'est dans ce but que j'ai créé ce site : me permettre de comprendre comment fonctionne un site,
-                    mais aussi mettre au point des applications web exploitant des systèmes comme des Raspberry par exemple.
-                    <BR />
-                    Vous pouvez de plus consulter <A HREF="CV.html">mon CV</A> si vous le souhaitez.
+                        
                 </p>
 
                 <p>
-                    Pour me contacter, vous pouvez m'envoyer un e-mail à <A HREF="mailto:nicolashavard.02@hotmail.fr" >mon adresse mail</A> ou bien consulter les différents sites suivants :
-                    <BR />
-                    <ul>
-                        <li><A HREF="https://www.facebook.com/nicolas.shuli.havard" ><img src="images/fb.png" onmouseover="this.src='images/fb_color.png';" onmouseout="this.src='images/fb.png';" width="50" height="50" /> Facebook</A></li>
-                        <li><A HREF="https://twitter.com/NicolasHavard" ><img src="images/twitter.png" onmouseover="this.src='images/twitter_color.png';" onmouseout="this.src='images/twitter.png';" width="50" height="50" /> Twitter</A></li>
-                        <li><A HREF="https://www.linkedin.com/in/nicolas-havard-7b964412a/" ><img src="images/linkedin.png" onmouseover="this.src='images/linkedin_color.png';" onmouseout="this.src='images/linkedin.png';" width="50" height="50" /> Linkedin</A></li>
-                        <li><A HREF="http://www.viadeo.com/p/0021m0ymns9cnl7j" ><img src="images/viadeo.png" onmouseover="this.src='images/viadeo_color.png';" onmouseout="this.src='images/viadeo.png';" width="50" height="50" /> Viadeo</A></li>
-                        <li><A HREF="https://github.com/Nicolas-Havard" ><img src="images/github.png" onmouseover="this.src='images/github_color.png';" onmouseout="this.src='images/github.png';" width="50" height="50" /> Github</A></li>
-                    </ul>    
+                    
                 </p>
             </article>
         </section>
         </div>
         
         <?php
-            include 'struct/footer.php';
+        include 'footer.php';
         ?>
-
+        
+        <footer>											
+            <A HREF="index.html"><em>Accueil</em></A>
+            <A href="mailto:nicolashavard.02@hotmail.fr">Me contacter par mail !</A>
+            <A HREF="">Me contacter</A>
+            <A HREF="">Mentions légales</A>
+        </footer>
+        
     </body>
 </html>
